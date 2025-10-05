1d4da3a41d6007daa9cb1de445c8e5904e9157c5
enzo-sparta <enzo@bamse.co>
2025-10-05 19:15:24 -0300

    add index

--- codigo-fonte/frontend/src/components/ui/index.js
+++ codigo-fonte/frontend/src/components/ui/index.js
@@ -5,3 +5,5 @@ export { default as Toast, ToastContainer } from './Toast';
 export { default as Logo } from './Logo';
 export { default as LoadingSpinner, FullPageLoader } from './LoadingSpinner';
 export { default as FeatureCard } from './FeatureCard';
+export { default as Header } from './PageHeader';
+export { default as Modal } from './Modal';
