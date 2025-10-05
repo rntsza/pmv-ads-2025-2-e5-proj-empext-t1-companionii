6f8f2ec3c09241c835e272aa02127411f9a3be36
William Rodrigues <williamrodrigues2442@gmail.com>
2025-10-05 12:42:43 -0300

    refactor: fixes seed and remove unused files

--- codigo-fonte/backend/src/timelog/dto/update-timeLogs.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTimeLogsDto } from './create-timeLogs.dto';
-
-export class UpdateTimeLogsDto extends PartialType(CreateTimeLogsDto) {}
